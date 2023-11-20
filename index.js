2d83444811b868bfae1514262f4b3e4a1c15970e
9ta1ls <rpeleh19@gmail.com>
2023-11-20 17:44:43 +0200

    meow

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const upload = multer();
 
 app.use(express.json());
 app.use("/static", express.static(path.join(__dirname, "static")));
+app.use(upload.any());
+app.use(express.text());
 
 app.get("/UploadForm.html", (req, res) => {
   res.sendFile(path.join(__dirname, "static", "UploadForm.html"));
@@ -17,14 +19,59 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "notes.json"));
 });
 
-app.get("/notes/:noteTitle", (req,res)=>{
-    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "notes.json")));
-    const noteTitle = req.params.noteTitle;
-    const foundNote = notes.find(obj => obj.title === noteTitle);
-    if(!foundNote)
-        res.sendStatus(404);
-    res.send(foundNote.note);
-})
+app.get("/notes/:noteTitle", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "notes.json")));
+  const noteTitle = req.params.noteTitle;
+  const foundNote = notes.find((obj) => obj.title === noteTitle);
+  if (!foundNote){
+    res.sendStatus(404);
+  }
+  res.send(foundNote.note);
+});
+
+app.post("/upload", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "notes.json")));
+  const noteTitle = req.body.note_name;
+  const foundNote = notes.find((obj) => obj.title === noteTitle);
+  if(foundNote){
+    res.sendStatus(400);
+  }
+  const Note = req.body.note;
+  const newNote = {"title": `${noteTitle}`, "note": `${Note}`};
+  notes.push(newNote);
+  dataToWrite = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataToWrite);
+  res.sendStatus(200);
+});
+
+app.put("/notes/:noteTitle", (req, res)=>{
+  let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "notes.json")));
+  const noteTitle = req.params.noteTitle;
+  const foundNote = notes.find((obj) => obj.title === noteTitle);
+  if (!foundNote){
+    res.sendStatus(404);
+  }
+  const newNote = req.body;
+  foundNote.note = newNote;
+  dataToWrite = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataToWrite);
+  console.log(notes);
+  res.sendStatus(200);
+});
+
+app.delete("/notes/:noteTitle", (req, res)=>{
+  let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "notes.json")));
+  const noteTitle = req.params.noteTitle;
+  const foundIndexNote = notes.findIndex((obj) => obj.title === noteTitle);
+  console.log(foundIndexNote);
+  if (foundIndexNote === -1){
+    res.sendStatus(404);
+  }
+  notes.splice(foundIndexNote, 1);
+  const dataToWrite = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataToWrite);
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущено на порті ${PORT}`);
