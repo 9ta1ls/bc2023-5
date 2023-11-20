489aee866cb4738a78be82429f2cd335f8b3c88a
9ta1ls <rpeleh19@gmail.com>
2023-11-20 12:32:46 +0200

    second commit

--- index.js
+++ index.js
@@ -7,19 +7,25 @@ const fs = require("fs");
 const upload = multer();
 
 app.use(express.json());
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use("/static", express.static(path.join(__dirname, "static")));
 
 app.get("/UploadForm.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "static", "UploadForm.html"));
+  res.sendFile(path.join(__dirname, "static", "UploadForm.html"));
 });
 
-app.get("/notes", (req,res)=>{
-    res.sendFile(path.join(__dirname,"notes.json"));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "notes.json"));
 });
 
-
-
-
-app.listen(PORT,()=>{
-    console.log(`Сервер запущено на порті ${PORT}`); 
+app.get("/notes/:noteTitle", (req,res)=>{
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "notes.json")));
+    const noteTitle = req.params.noteTitle;
+    const foundNote = notes.find(obj => obj.title === noteTitle);
+    if(!foundNote)
+        res.sendStatus(404);
+    res.send(foundNote.note);
 })
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущено на порті ${PORT}`);
+});
